3714e5e8fb3c9238124fefcfc219899b026025f3
marslanabbas <muhammadarslanabbas960@gmail.com>
2026-01-27 18:47:04 +0500

    02-components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+      <Card/>
+      <Card/>
+      <Card/>
+      
+
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className="card">
+        <h1>Muhammad Arslan Abbas</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, et.</p>
+     </div>
+    </div>
+  )
+}
+
+export default Card
