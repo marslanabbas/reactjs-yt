5e6544fe167d3b2b79824a78301a3463c4307f5a
marslanabbas <muhammadarslanabbas960@gmail.com>
2026-01-31 12:09:20 +0500

    some chnages in css file structures

---
+++ 05-css/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import second from '../styles/button.css'
+const button = () => {
+  return (
+    <div className="btn">
+        This is Button
+    </div>
+  )
+}
+
+export default button
