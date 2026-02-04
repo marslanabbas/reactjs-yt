23f8964e1aba97b63df4ee383b515fe0c3adf671
marslanabbas <muhammadarslanabbas960@gmail.com>
2026-02-04 21:38:05 +0500

    checking UI

--- 07-ui-project/src/App.jsx
+++ 07-ui-project/src/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 
-const App = () => {
+function App() {
+  const userName = 'Alex';
+  const appTitle = 'My First React UI Project';
+
+  
   return (
-    <div >
+    <div className="App">
+     
+      <h1>Hello, {userName}! Welcome to {appTitle}</h1>
+      
+      <img 
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="A description of the image" 
+        className="app-logo" 
+      />
+
+      
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+
+      
       
     </div>
-  )
+  );
 }
 
 export default App
